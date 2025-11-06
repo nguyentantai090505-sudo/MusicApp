bf907791d7ef879553693b4e549513da359a9845
nguyentantai090505-sugit config --global user.email <nguyentantai090505@gmail.com>
2025-11-06 16:08:58 +0700

    Update: chỉnh sửa dependencies trong app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,12 +57,12 @@ dependencies {
 
 
     // === GOOGLE SIGN-IN ===
-    implementation("com.google.android.gms:play-services-auth:20.7.0")
+    implementation("com.google.android.gms:play-services-auth:21.2.0")
 
     // === HILT ===
-    implementation("com.google.dagger:hilt-android:2.48.1")
-    kapt("com.google.dagger:hilt-android-compiler:2.4.1")
-    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.51.1")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
     // === EXOPLAYER (Media3)
     implementation("androidx.media3:media3-exoplayer:1.3.1")
@@ -71,23 +71,30 @@ dependencies {
     implementation("androidx.media3:media3-common:1.3.1")
 
     // === COMPOSE
-    implementation("androidx.compose.ui:ui:1.5.4")
+    implementation("androidx.compose.ui:ui:1.5.11")
     implementation("androidx.compose.material3:material3:1.1.2")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.5.4")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.11")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.navigation:navigation-compose:2.7.6")
 
     // === COROUTINES ===
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
+    // ==== COIL ===
+    implementation("io.coil-kt:coil-compose:2.6.0")
+    implementation("androidx.compose.material3:material3:1.2.1")
     // === RETROFIT (API)
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     // === TEST ===
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.4")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.1")
+    // ==kotlin==
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.23")
+}
+kapt{
+    correctErrorTypes = true
 }
